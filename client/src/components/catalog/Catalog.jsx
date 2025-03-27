a2b1b35db6f1c524229a47e645344367e5f3fd1e
Misho1019 <mikailtorres99@gmail.com>
2025-03-27 19:55:18 +0200

    Empty boots screen

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -7,14 +7,17 @@ export default function Catalog() {
 
     useEffect(() => {
         bootService.getAll()
-            .then(setBoots)
+            .then([])
     }, [])
 
     return (
         <div className="catalog-container">
             <h2 className="catalog-heading">Football Boots Collection</h2>
             <div className="boots-grid">
-                {boots.map(boot => <BootItem key={boot._id} {...boot}/>)}
+                {boots.length > 0
+                    ? boots.map(boot => <BootItem key={boot._id} {...boot} />)
+                    : <h3 className="no-boots">No Football Boots Yet</h3>
+                }
             </div>
         </div>
     );
