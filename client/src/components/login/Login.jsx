8dc7d93aeb759bc8f19a2e038e3c9f95395fb7da
Misho1019 <mikailtorres99@gmail.com>
2025-03-27 02:55:34 +0200

    Use action state for login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,14 +1,31 @@
-export default function Login() {
+import { useActionState } from "react";
+import { Link, useNavigate } from "react-router";
+
+export default function Login({
+    onLogin,
+}) {
+    const navigate = useNavigate()
+
+    const loginHandler = (_, formData) => {
+        const values = Object.fromEntries(formData)
+
+        onLogin(values.email)
+
+        navigate('/boots')
+    }
+
+    const [_, loginAction, isPending] = useActionState(loginHandler, { email: '', password: '' })
+
     return (
         <div className="auth-container">
             <div className="auth-box">
                 <h2>Login</h2>
-                <form>
+                <form action={loginAction}>
                     <input type="email" placeholder="Email" required />
                     <input type="password" placeholder="Password" required />
-                    <button type="submit">Login</button>
+                    <button type="submit" disabled={isPending}>Login</button>
                 </form>
-                <p>Don't have an account? <a href="/register">Sign up</a></p>
+                <p>Don't have an account? <Link href="/register">Sign up</Link></p>
             </div>
         </div>
     );
