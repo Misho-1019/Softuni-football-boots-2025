786ae44ceaf41e9ddb561b9750dbaee8de86478f
Misho1019 <mikailtorres99@gmail.com>
2025-03-28 18:42:38 +0200

    Redirect on successful logout

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import request from "../utils/request";
 import { UserContext } from "../context/userContext";
 
@@ -27,17 +27,25 @@ export const useRegister = () => {
 }
 
 export const useLogout = () => {
-    const { accessToken } = useContext(UserContext)
+    const { accessToken, userLogoutHandler } = useContext(UserContext)
 
-    const options = {
-        headers: {
-            'X-Authorization': accessToken,
+    useEffect(() => {
+        if (!accessToken) {
+            return
         }
-    }
 
-    const logout = () => request.get(`${baseUrl}/logout`, null, options)
+        const options = {
+            headers: {
+                'X-Authorization': accessToken,
+            }
+        }
+
+        request.get(`${baseUrl}/logout`, null, options)
+            .then(userLogoutHandler)
+
+    }, [accessToken, userLogoutHandler])
 
     return {
-        logout,
+        isLoggedOut: !!accessToken,
     }
 }
