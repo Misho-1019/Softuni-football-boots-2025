e9729f1d4ba49a15bb996efb0141d8c14d1240b1
Misho1019 <mikailtorres99@gmail.com>
2025-03-28 06:38:00 +0200

    Add user register

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,14 +1,41 @@
+import { Link, useNavigate } from "react-router";
+import { useRegister } from "../../api/authApi";
+import { useContext } from "react";
+import { UserContext } from "../../context/userContext";
+
 export default function Register() {
+    const navigate = useNavigate();
+    const { register } = useRegister()
+    const { userLoginHandler } = useContext(UserContext)
+
+    const registerHandler = async (formData) => {
+        const { email, password } = Object.fromEntries(formData)
+
+        const confirmPassword = formData.get('confirm-password')
+
+        if (password !== confirmPassword) {
+            console.log('Password mismatch!');
+            
+            return;
+        }
+
+        const authData = await register(email, password)
+
+        userLoginHandler(authData)
+
+        navigate('/boots')
+    }
     return (
         <div className="auth-container">
             <div className="auth-box">
                 <h2>Register</h2>
-                <form>
-                    <input type="email" placeholder="Email" required />
-                    <input type="password" placeholder="Password" required />
+                <form action={registerHandler}>
+                    <input type="email" name="email" placeholder="Email" required />
+                    <input type="password" name="password" placeholder="Password" required />
+                    <input type="password" name="confirm-password" placeholder="Confirm Password" required />
                     <button type="submit">Register</button>
                 </form>
-                <p>Already have an account? <a href="/login">Login</a></p>
+                <p>Already have an account? <Link to="/login">Login</Link></p>
             </div>
         </div>
     );
