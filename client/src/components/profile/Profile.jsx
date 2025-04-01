e211e5f51b0566d07617c5229b710ac756401272
Misho1019 <mikailtorres99@gmail.com>
2025-04-02 02:45:11 +0300

    Add functionality in profile

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,22 +1,31 @@
+import { useState } from "react"
 import useAuth from "../../hooks/useAuth"
 
 export default function Profile() {
-    const { email, accessToken, _id, username } = useAuth()
+    const { email, _id, username } = useAuth()
+    const [showPassword, setShowPassword] = useState(false)
 
-    console.log(email, accessToken, _id, username);
-    
     return (
         <div className="profile-container">
             <div className="profile-card">
                 <div className="profile-card">
                     <h2 className="title">Profile</h2>
-                    <div className="input-group">
-                        <label className="label">Email</label>
-                        <input type="email" className="input" />
-                    </div>
-                    <div className="input-group">
-                        <label className="label">Password</label>
-                        <input type="password" className="input" />
+                    <div className="profile-info">
+                        <div className="input-group">
+                            <label className="label">Username</label>
+                            <input type="email" className="input" defaultValue={username} />
+                        </div>
+                        <div className="input-group">
+                            <label className="label">Email</label>
+                            <input type="email" className="input" defaultValue={email} />
+                        </div>
+                        <div className="password-group">
+                            <label className="password-label">User Id</label>
+                            <div className="show-password">
+                                <input type={showPassword ? 'text' : 'password'} className="input" defaultValue={_id} />
+                                <button onClick={() => setShowPassword(!showPassword)}>👁️</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
