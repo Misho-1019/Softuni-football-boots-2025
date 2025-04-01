a2106880190b6cb3fed1b24d56cb82aad5f68fab
Misho1019 <mikailtorres99@gmail.com>
2025-04-02 01:55:23 +0300

    Add username in register form

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -8,7 +8,7 @@ export default function Register() {
     const { userLoginHandler } = useUserContext()
 
     const registerHandler = async (formData) => {
-        const { email, password } = Object.fromEntries(formData)
+        const { username, email, password } = Object.fromEntries(formData)
 
         const confirmPassword = formData.get('confirm-password')
 
@@ -18,7 +18,7 @@ export default function Register() {
             return;
         }
 
-        const authData = await register(email, password)
+        const authData = await register(username, email, password)
 
         userLoginHandler(authData)
 
@@ -29,6 +29,7 @@ export default function Register() {
             <div className="auth-box">
                 <h2>Register</h2>
                 <form action={registerHandler}>
+                    <input type='text' name="username" placeholder="Username" required />
                     <input type="email" name="email" placeholder="Email" required />
                     <input type="password" name="password" placeholder="Password" required />
                     <input type="password" name="confirm-password" placeholder="Confirm Password" required />
